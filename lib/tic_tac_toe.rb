ac05905b15b5305aacb222280bb1d99a9cd7fc17
Adam Ye <adamye48@gmail.com>
2020-08-12 21:19:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,6 @@
-#def position_taken?(board, index)
-#  !(board[index].nil? || board[index] == " ")
-#end
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
